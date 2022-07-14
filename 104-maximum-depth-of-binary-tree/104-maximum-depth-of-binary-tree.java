7b0aa4522ba216c5d71d7de22eb85983dbc109c7
Akanksha Sharma <68657899+akankshaSha@users.noreply.github.com>
2022-07-14 23:43:47 +0530

    Time: 7 ms (8.74%), Space: 43.2 MB (48.61%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
@@ -15,9 +15,44 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
+        if(root==null) return 0;
+        
+        Stack <TreeNode> nodes =new Stack<>();
+        Stack <Integer> d=new Stack<>();
+        
+        nodes.push(root);
+        d.push(1);
+        int max=0;
+        
+        while(!nodes.empty())
+        {
+            TreeNode t=nodes.pop();
+            int h=d.pop();
+            if(t.left==null && t.right==null)
+            {
+                max=Math.max(max, h);
+                continue;
+            }
+            if(t.left!=null)
+            {
+                nodes.push(t.left);
+                d.push(h+1);
+            }
+            if(t.right!=null)
+            {
+                nodes.push(t.right);
+                d.push(h+1);
+            }
+        }
+        return max;
+    }
+}
+
+/* // recursive soln:
+    public int maxDepth(TreeNode root) {
        if(root==null) return 0;
         int left=maxDepth(root.left);
         int right=maxDepth(root.right);
         return 1+ Math.max(left, right);
     }
-}
+*/
