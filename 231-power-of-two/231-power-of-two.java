6c06dfdccc28835bb7174bfb846a6838fb8b256e
Akanksha Sharma <68657899+akankshaSha@users.noreply.github.com>
2022-07-27 22:22:15 +0530

    Time: 2 ms (68.06%), Space: 41.2 MB (55.89%) - LeetHub

--- 231-power-of-two/231-power-of-two.java
+++ 231-power-of-two/231-power-of-two.java
@@ -1,8 +1,13 @@
 class Solution {
-    public boolean isPowerOfTwo(int n) {
+    //recursive solution:
+    /*public boolean isPowerOfTwo(int n) {
         if(n==0) return false;
         if(n==1) return true;
         if(n%2==1) return false;
         return isPowerOfTwo(n/2);
+    }*/
+    public boolean isPowerOfTwo(int n) {
+        if(n==0 || n==Integer.MIN_VALUE) return false;
+        return (n&(n-1))==0;
     }
 }
