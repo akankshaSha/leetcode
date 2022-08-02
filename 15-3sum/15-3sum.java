c0784193bced3bd1c8ec8badf76b1644f9ccb61d
Akanksha Sharma <68657899+akankshaSha@users.noreply.github.com>
2022-08-02 11:27:24 +0530

    Time: 217 ms (25.79%), Space: 58.7 MB (65.90%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -2,21 +2,18 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res=new ArrayList<>();
         Arrays.sort(nums);
-        for(int i=0; i<nums.length; i++)
+        for(int i=0; i<nums.length-1; i++)
         {
             if(i>0 && nums[i]==nums[i-1]) continue;
-            for(int j=i+1; j<nums.length; j++)
+            for(int j=i+1; j<nums.length-1; j++)
             {
-                if(j-1!=i && nums[j]==nums[j-1]) continue;
-                int target=(nums[i]+nums[j])*-1;
+                if(nums[j]==nums[j-1] && j!=i+1) continue;
+                int target=-(nums[i]+nums[j]);
                 int k=Arrays.binarySearch(nums, j+1, nums.length, target);
                 if(k>=0)
                 {
-                    List<Integer> aux=new ArrayList<>();
-                    aux.add(nums[i]);
-                    aux.add(nums[j]);
-                    aux.add(nums[k]);
-                    res.add(aux);
+                    List<Integer> triplet=Arrays.asList(nums[i], nums[j], nums[k]);
+                    res.add(triplet);
                 }
             }
         }
