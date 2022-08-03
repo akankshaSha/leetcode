ef924526312c1cb2f2f1f1b6aa1ac5117ab8ea27
Akanksha Sharma <68657899+akankshaSha@users.noreply.github.com>
2022-08-03 22:34:11 +0530

    Time: 27 ms (5.96%), Space: 58.7 MB (5.33%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,26 +1,33 @@
 class Solution {
-    
-    public void demark(int i, int j, char[][] grid)
-    {
-        grid[i][j]='0';
-        if(i+1<grid.length && grid[i+1][j]=='1') demark(i+1, j, grid);
-        if(i-1>-1 && grid[i-1][j]=='1') demark(i-1, j, grid);
-        if(j+1<grid[0].length && grid[i][j+1]=='1') demark(i, j+1, grid);
-        if(j-1>-1 && grid[i][j-1]=='1') demark(i, j-1, grid);
-    }
     public int numIslands(char[][] grid) {
-        int count=0;
-        for(int i=0; i<grid.length; i++)
-        {
-            for(int j=0; j<grid[i].length; j++)
-            {
-                if(grid[i][j]=='1')
-                {
-                    demark(i, j, grid);
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
+	if (grid == null || grid.length == 0) {
+	  return 0;
+	}
+	int nr = grid.length;
+	int nc = grid[0].length;
+	int num_islands = 0;
+	Queue<int[]> queue=new LinkedList<>();
+	for(int r=0;r<nr;r++){
+		for(int c=0;c<nc;c++){
+			if(grid[r][c]=='1'){
+				queue.add(new int[]{r,c});
+				num_islands++;
+				while(!queue.isEmpty()){
+					int[] curr=queue.poll();
+					int row=curr[0];
+					int col=curr[1];
+					if(row<0 || col<0 || row>=nr || col>=nc || grid[row][col]=='0'){
+						continue;
+					}
+					grid[row][col]='0';
+					queue.add(new int[]{row+1,col});
+					queue.add(new int[]{row-1,col});
+					queue.add(new int[]{row,col+1});
+					queue.add(new int[]{row,col-1});
+				}
+			}
+		}
+	}
+	return num_islands;
+}
 }
