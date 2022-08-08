ba95a4684313d6ab20558ebb1000964c25a6ac85
Akanksha Sharma <68657899+akankshaSha@users.noreply.github.com>
2022-08-08 11:22:15 +0530

    Time: 199 ms (12.55%), Space: 54.5 MB (14.02%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -0,0 +1,59 @@
+class Solution {
+    public boolean inclusion(HashMap<Character, Integer>ft, HashMap<Character, Integer>fs)
+    {
+        for(char c: ft.keySet())
+        {
+            if(fs.get(c)<ft.get(c)) return false;
+        }
+        return true;
+    }
+    
+    
+    public String minWindow(String s, String t) {
+        int start=0, end=0, minLen=Integer.MAX_VALUE;
+        String res="";
+        HashMap<Character, Integer> ft=new HashMap<>();
+        HashMap<Character, Integer> fs=new HashMap<>();
+        for(int i=0; i<t.length(); i++)
+        {
+            char c=t.charAt(i);
+            ft.put(c, ft.getOrDefault(c, 0)+1);
+            fs.put(c, 0);
+        }
+        
+        for(; end<s.length(); end++)
+        {
+            char c=s.charAt(end);
+            if(fs.containsKey(c)) fs.put(c, fs.get(c)+1);
+            //System.out.println(start+" "+" "+end+" "+fs+" "+ft+" "+inclusion(ft, fs));
+            while(inclusion(ft, fs))
+            {
+                int len=end-start+1;
+                if(len<minLen)
+                {
+                    minLen=len;
+                    res=s.substring(start, end+1);
+                }
+                char ch=s.charAt(start);
+                if(fs.containsKey(ch)) fs.put(ch, fs.get(ch)-1);
+                start++;
+            }
+        }
+        
+        for(; start<s.length(); start++)
+        {
+            if(inclusion(ft, fs))
+            {
+                int len=end-start+1;
+                if(len<minLen)
+                {
+                    minLen=len;
+                    res=s.substring(start, end+1);
+                }
+                char ch=s.charAt(start);
+                if(fs.containsKey(ch)) fs.put(ch, fs.get(ch)-1);
+            }
+        }
+        return res;
+    }
+}
