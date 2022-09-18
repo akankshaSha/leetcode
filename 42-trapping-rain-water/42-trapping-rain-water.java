124deec2b39a160bfbeaadcb42b755cd6ab3d41f
Akanksha Sharma <68657899+akankshaSha@users.noreply.github.com>
2022-09-18 07:40:26 +0530

    Time: 2 ms (61.44%), Space: 49.9 MB (9.00%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,43 +1,24 @@
 class Solution {
     public int trap(int[] height) {
-        int left=0, right=height.length-1, water=0, maxLeft=height[left], maxRight=height[right];
-        while(left<=right)
+        int water=0;
+        int leftmax[]=new int[height.length];
+        int rightmax[]=new int[height.length];
+        
+        leftmax[0]=height[0];
+        for(int i=1; i<height.length; i++)
         {
-            if(maxLeft<maxRight)
-            {
-                int h=maxLeft-height[left];
-                if(h>0) water+=h;
-                maxLeft=Math.max(maxLeft, height[left]);
-                left++;
-            }
-            else
-            {
-                int h=maxRight-height[right];
-                if(h>0) water+=h;
-                maxRight=Math.max(maxRight, height[right]);
-                right--;
-            }
+            leftmax[i]=Math.max(leftmax[i-1], height[i]);
         }
-        return water;
-    }
-}
-
-//O(n) space solution:
-/*
-class Solution {
-    public int trap(int[] height) {
-        int maxLeft[]=new int[height.length], maxRight[]=new int[height.length], water=0;
-        maxLeft[0]=height[0];
-        for(int i=1; i<height.length; i++) maxLeft[i]=Math.max(maxLeft[i-1], height[i]);
-        maxRight[maxRight.length-1]=height[height.length-1];
-        for(int i=maxRight.length-2; i>=0; i--) maxRight[i]=Math.max(maxRight[i+1], height[i]);
+        rightmax[rightmax.length-1]=height[height.length-1];
+        for(int i=height.length-2; i>=0; i--)
+        {
+            rightmax[i]=Math.max(rightmax[i+1], height[i]);
+        }
         
         for(int i=0; i<height.length; i++)
         {
-            int h=Math.min(maxRight[i], maxLeft[i])-height[i];
-            if(h>0) water+=h;
+            water+=Math.min(rightmax[i], leftmax[i])-height[i];
         }
         return water;
     }
 }
-*/
