239b226b0abe5b81b07c94f5d365e188b0f2b00b
Akanksha Sharma <68657899+akankshaSha@users.noreply.github.com>
2022-09-26 12:51:14 +0530

    Time: 2 ms (69.35%), Space: 42.8 MB (29.84%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.java
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.java
@@ -1,10 +1,59 @@
+// disjont sets union find
+class Solution {
+    int find(int values[], int x)
+    {
+        return values[x]==x?x:find(values, values[x]);
+    }
+    
+    public boolean equationsPossible(String[] equations) {
+        int values[]=new int[26];
+        for(int i=0; i<26; i++) values[i]=i;
+        for(String equation: equations)
+        {
+            int a=equation.charAt(0)-'a';
+            int b=equation.charAt(3)-'a';
+            boolean equals=equation.charAt(1)=='='? true: false;
+            if(equals)
+            {
+                int A=find(values, a);
+                values[a]=A;
+                int B=find(values, b);
+                values[b]=B;
+                if(A!=B)
+                {
+                    if(A<B) values[B]=A;
+                    else values[A]=B;
+                }
+            }
+        }
+        
+        for(String equation: equations)
+        {
+            int a=equation.charAt(0)-'a';
+            int b=equation.charAt(3)-'a';
+            boolean equals=equation.charAt(1)=='='? true: false;
+            if(!equals)
+            {
+                if(find(values, a)==find(values, b)) return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+
+
+
+// naive approach I am embarresed of, but hey! at least I got the problem right!
+/*
 class Solution {       
     public boolean equationsPossible(String[] equations) {
         int val[]=new int[26], distinct=1;
         for(String equation: equations)
         {
-            char a=equation.charAt(0);
-            char b=equation.charAt(3);
+            int a=equation.charAt(0)-'a';
+            int b=equation.charAt(3)-'a';
             boolean equals=equation.charAt(1)=='='? true: false;
             if(equals)
             {
@@ -33,3 +82,4 @@ class Solution {
         return true;
     }
 }
+*/
