d566aaec4deda598e27f648c74ccff875e1e3974
Akanksha Sharma <68657899+akankshaSha@users.noreply.github.com>
2022-11-08 06:45:40 +0530

    Time: 3 ms (93.08%), Space: 42.4 MB (80.71%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,16 +1,16 @@
 class Solution {
     public String makeGood(String s) {
-        String res = "";
+        StringBuilder res = new StringBuilder();
         for(int i =0; i<s.length(); i++)
         {
-            if(res.length() == 0) res += s.charAt(i);
+            if(res.length() == 0) res.append(s.charAt(i));
             else
             {
                 char a = s.charAt(i), b = res.charAt(res.length()-1);
-                if(Math.abs(a - b) == 32) res = res.substring(0, res.length()-1);
-                else res += a;
+                if(Math.abs(a - b) == 32) res = res.deleteCharAt(res.length()-1);
+                else res.append(a);
             }
         }
-        return res;
+        return res.toString();
     }
 }
